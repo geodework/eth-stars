a804f6bc664b12506b51dc4944bb104e78a6a161
toshiboy86 <toshibowie86@gmail.com>
2025-05-27 10:56:11 +0900

    feat: added weather fetch API and component

---
+++ api/weather/config.ts
@@ -0,0 +1,6 @@
+export const WEATHER_API_CONFIG = {
+  baseUrl:
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline",
+  apiKey: process.env.WEATHER_API_KEY,
+  unitGroup: "metric",
+} as const
